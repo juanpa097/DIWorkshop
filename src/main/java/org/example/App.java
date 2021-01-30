fbdff5b3f63c6fdaec044363f7d11b6177b93ce3
Juan Penaloza <juanpenaloza@fb.com>
2021-01-30 10:05:07 +0000

    initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
