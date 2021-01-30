aee8ef24c0bfd8d1a4858aea6b9e881d9f3a6f20
Juan Penaloza <juanpenaloza@fb.com>
2021-01-30 10:07:42 +0000

    lint

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,5 @@
 package org.example;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
