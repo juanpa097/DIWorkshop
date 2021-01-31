cef6eb6cd740dfa293c610109dee65a48cd7f826
Juan Penaloza <juanpenaloza@fb.com>
2021-01-31 10:01:23 +0000

    Created Main.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,8 +1,20 @@
 package org.example;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Path taskFilePath = Paths.get("src/main/resources/MyTasks.txt");
+        Repository repository = new TaskRepository(taskFilePath);
+
+        ShowTaskList showTaskList = new ShowTaskList(repository);
+
+        String[] tasks = showTaskList.fetchTasks();
+
+        for (String task : tasks) {
+            System.out.println(task);
+        }
     }
 }
 
